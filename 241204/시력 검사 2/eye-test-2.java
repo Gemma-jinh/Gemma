cb692c604c3eff2726c4eea0df92a401e8298e4f
jin_h <137394389+Gemma-jinh@users.noreply.github.com>
2024-12-04 10:14:12 +0900

    Title: 시력 검사 2, Time: 143ms, Memory: 11MB, Status: Passed - CodeTree

--- 241204/시력 검사 2/eye-test-2.java
+++ 241204/시력 검사 2/eye-test-2.java
@@ -4,9 +4,9 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
-        if (a > 1.0) {
+        if (a >= 1.0) {
             System.out.println("High");
-        } else if (a >0.5) {
+        } else if (a >=0.5) {
             System.out.println("Middle");
         } else {
             System.out.println("Low");
