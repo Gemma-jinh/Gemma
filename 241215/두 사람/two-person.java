03cc9ef62fe6714becaa4e5ede0f2fceefc78d5a
jin_h <137394389+Gemma-jinh@users.noreply.github.com>
2024-12-15 13:08:24 +0900

    Title: 두 사람, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 241215/두 사람/two-person.java
+++ 241215/두 사람/two-person.java
@@ -7,7 +7,7 @@ public class Main {
         char gender_a = sc.next().charAt(0);
         int age_b = sc.nextInt();
         char gender_b = sc.next().charAt(0);
-        if ((age_a >=19 || age_b >=19) && (gender_a == 'M' || gender_b == 'M')) {
+        if ((age_a >=19 && gender_a == 'M') || (age_b >=19 && gender_b == 'M')) {
             System.out.println(1);
         } else {
             System.out.println(0);
