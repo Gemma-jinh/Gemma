db2f86aa6a89fd77ebc106e42e3e4dcca080b929
jin_h <137394389+Gemma-jinh@users.noreply.github.com>
2024-12-15 13:06:48 +0900

    Title: 두 사람, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241215/두 사람/two-person.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int age_a = sc.nextInt();
+        char gender_a = sc.next().charAt(0);
+        int age_b = sc.nextInt();
+        char gender_b = sc.next().charAt(0);
+        if ((age_a >=19 || age_b >=19) && (gender_a == 'M' || gender_b == 'M')) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
