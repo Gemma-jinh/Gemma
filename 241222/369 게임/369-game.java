d7b83868f186ec4376b70267bcdb4ac3cab7ffe3
jin_h <137394389+Gemma-jinh@users.noreply.github.com>
2024-12-22 14:02:02 +0900

    Title: 369 게임, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 241222/369 게임/369-game.java
+++ 241222/369 게임/369-game.java
@@ -7,9 +7,13 @@ public class Main {
         for (int i =1; i<=n; i++) {
             if( i%3 == 0 || i%10 ==3 || i%10 == 6 || i%10 == 9) {
                 System.out.print(0 + " ");
-            } else if( i ==100) {
-                System.out.print(i + " ");
-               } else {
+            } else if (i>=30 && i<40) {
+                System.out.print(0 + " ");
+            } else if (i>=60 && i<70) {
+                System.out.print(0 + " ");
+            } else if (i>=90 && i<100) {
+                System.out.print(0 + " ");
+            } else {
                 System.out.print(i + " ");
             }
         }
